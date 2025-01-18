36f4760ddd1b11bdc43e1e4c3351af75f19f766a
jim <zijin@ualberta.ca>
2025-01-17 18:02:54 -0800

    Added get and post routes for baskets

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,28 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const host = "localhost";
 const port = "8080";
 
+import { getBaskets, createBasket } from "./lib/database/pg.js";
+
 // app.use(express.static("public"));
 // app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("yeeeeeeeeee");
+app.get("/api/baskets", async (req, res) => {
+  const baskets = await getBaskets();
+  res.status(200).send({ baskets });
+});
+
+app.post("/api/baskets", async (req, res) => {
+  const name = req.body.name;
+  const dbName = await createBasket(name);
+
+  if (dbName === -1) {
+    res.status(409).send({ error: `Name ${name} already exists.` });
+  } else {
+    res.status(200).send(name);
+  }
 });
 
 app.listen(port, host, () => {
