10d1cb431d7c171a6f53a4986fe09d6f752c6716
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-01-18 14:17:48 -0600

    move seed data to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,30 @@
 // import { useState } from 'react'
 import './index.css'
 
+const baskets = [
+    "basket1",
+    "basket2",
+    "basket3"
+  ]
+
+const basket1 = {
+    "requests": [
+      {
+        "headers": "Accept: */* \nAccept-Encoding: gzip, deflate \nConnection: close \nUser-Agent: HTTPie/3.2.4 \nX-City: La Crosse \nX-Country: US \nX-Forwarded-For: 184.97.26.131 \nX-Real-Ip: 184.97.26.131",
+        "method": "GET",
+        "query": "hello=world&laren=tired",
+        "body": "",
+        "date_time": 1737224774543
+      },
+      {
+        "headers":"Accept: */* \nAccept-Encoding: gzip, deflate \nConnection: close \nUser-Agent: HTTPie/3.2.4 \nX-City: La Crosse \nX-Country: US \nX-Forwarded-For: 184.97.26.131 \nX-Real-Ip: 184.97.26.131",
+        "method": "POST",
+        "query": "",
+        "body": {"id":13399049247,"kind":"comment_created"}
+      }
+    ]
+}
+
 function Header() {
   return (
     <div className='header'>
