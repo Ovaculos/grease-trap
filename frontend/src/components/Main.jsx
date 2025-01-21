6ff45ce4914a0fe8bb343334d0b43dd1192bac56
Laren Houstoun <larenhoustoun@Mac.attlocal.net>
2025-01-20 21:48:36 -0600

    Add frontend validation for new basket input

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -32,20 +32,40 @@ function Main({ currBasket, baskets, setBaskets }) {
   //  => make sure it's not already in db
   // => call with useEffect and set basketName to this state
 
+  const isValidBasketName = (name) => {
+    if (baskets.includes(name)) {
+      alert("That basket name is already present. Try another name!");
+      return false;
+    }
+
+    if (!/^[\w\d\-_\.]{1,250}$/.test(name)) {
+      alert("Your basket name should be in this pattern");
+      return false;
+    }
+
+    return true;
+  }
+
   const addNewBasket = (e) => {
     e.preventDefault();
-    // change to actually making a post request
     
-    setBaskets(baskets.concat([basketName]));
+    // frontend validation
+    if (isValidBasketName(basketName)) {
+      // change to actually making a post request
+    
+      setBaskets(baskets.concat([basketName]));
+    }
   }
 
   if (!currBasket) { //!basket
     return (
       <div className='main'>
         <form action="" onSubmit={(e) => addNewBasket(e)}>
-          <input placeholder='Basket name'
+          <input type="text"
+                 placeholder='Basket name'
                  value={basketName}
                  onChange={(e) => setNewBasket(e.target.value)}
+                 pattern="^[\w\d\-_\.]{1,250}$"
            />
           <input type="submit" value="Create Basket" />
         </form>
