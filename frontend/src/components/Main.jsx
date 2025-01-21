42c675ebfddebd59720d3d5d433a98e26dd9b621
Laren Houstoun <larenhoustoun@Mac.attlocal.net>
2025-01-20 22:06:21 -0600

    Remove comments from Main Component

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,33 +1,12 @@
 /* eslint-disable react/prop-types */
-// import { useEffect, useState } from 'react'
 import { useState } from 'react';
 import './Main.css';
 import RequestList from './RequestList';
 
-/*
-Planning:
 
-Two Main Pages:
-
-1. Home Page => Add a new basket
-  - if state of currBasker is empty string show homepage
-
-
-2. Basket Page => Basket info + List of Requests
-  - if state of currBasket is a basket show that basket instead
-
-*/
-
-const baskets = [
-  "basket1",
-  "basket2",
-  "basket3"
-];
-
-
-// eslint-disable-next-line react/prop-types
 function Main({ currBasket, baskets, setBaskets }) {
   const [basketName, setNewBasket] = useState("");
+  
   // create a random url generator 
   //  => make sure it's not already in db
   // => call with useEffect and set basketName to this state
@@ -38,7 +17,7 @@ function Main({ currBasket, baskets, setBaskets }) {
       return false;
     }
 
-    if (!/^[\w\d\-_\.]{1,250}$/.test(name)) {
+    if (!/^[\w\d\-_.]{1,250}$/.test(name)) {
       alert("Your basket name should be in this pattern");
       return false;
     }
@@ -48,8 +27,7 @@ function Main({ currBasket, baskets, setBaskets }) {
 
   const addNewBasket = (e) => {
     e.preventDefault();
-    
-    // frontend validation
+
     if (isValidBasketName(basketName)) {
       // change to actually making a post request
     
@@ -57,7 +35,7 @@ function Main({ currBasket, baskets, setBaskets }) {
     }
   }
 
-  if (!currBasket) { //!basket
+  if (!currBasket) {
     return (
       <div className='main'>
         <form action="" onSubmit={(e) => addNewBasket(e)}>
@@ -65,7 +43,7 @@ function Main({ currBasket, baskets, setBaskets }) {
                  placeholder='Basket name'
                  value={basketName}
                  onChange={(e) => setNewBasket(e.target.value)}
-                 pattern="^[\w\d\-_\.]{1,250}$"
+                //  pattern="^[\w\d\-_\.]{1,250}$"
            />
           <input type="submit" value="Create Basket" />
         </form>
@@ -78,8 +56,6 @@ function Main({ currBasket, baskets, setBaskets }) {
       </div>
     )
   }
-
-
 }
 
 
