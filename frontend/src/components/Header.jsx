15d9897f53e359947d217646c4ada758ceffafed
Laren Houstoun <larenhoustoun@Mac.attlocal.net>
2025-01-22 16:05:22 -0600

    Add delete basket temp code

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,38 @@
 // eslint-disable-next-line react/prop-types
 function Header({ basket, returnToHome}) {
-  return (
-    <div className='header'>
-      <a  onClick={returnToHome}>HOME</a>
-      <h1>{basket ? basket : "Welcome to GREASETRAP!"}</h1>
-    </div>
-  )
+  const deleteBasket = async () => {
+    try {
+      console.log("delete basket")
+      // 1. warning
+
+      // 2. api call
+
+      // 3. set current basket to empty text to return home
+      
+    } catch (error) {
+      console.error("Error deleting basket:", error);
+    }
+  }
+
+
+  if (basket) {
+    return (
+      <div className='header'>
+        <a  onClick={returnToHome}>HOME</a>
+        <h1>{basket}</h1>
+        <button onClick={() => deleteBasket()}>Delete Basket</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className='header'>
+        <a  onClick={returnToHome}>HOME</a>
+        <h1>{"Welcome to GREASETRAP!"}</h1>
+      </div>
+    )
+  }
 }
 
 export default Header;
+
+
