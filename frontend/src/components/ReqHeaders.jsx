a33541b676b4c4b6524b1adf3d5267d1d9fc6a4b
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-01-22 21:11:54 -0600

    add sidebar and request header auto-scaling, add tertiary color for button hover

--- frontend/src/components/ReqHeaders.jsx
+++ frontend/src/components/ReqHeaders.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 /* eslint-disable react/prop-types */
 function ReqHeaders( { headers }) {
   const [isOpen, setIsOpen] = useState(false);
+  console.log(headers);
 
   return (
     <>
       <button onClick={() => setIsOpen(!isOpen)}>Headers</button>
       <div className="info-dropdown">
-        {isOpen && <p>{headers}</p>}
+        {isOpen && <pre id="json">{headers}</pre>}
       </div>
     </>
 
